5319375511882ccbb976da40bb2941d6638d9205
xyperine <ewsannne@gmail.com>
2025-03-04 06:33:25 +0300

    Improved fps on mobile

---
+++ Assets/Scripts/FramerateSetter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DragnDrop
+{
+    public class FramerateSetter : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [SerializeField] private bool workInEditor;
+#endif
+        [SerializeField, Range(0, 60)] private int targetFramerate = 60;
+        
+        
+        private void Awake()
+        {
+            SetFramerate();
+        }
+
+
+        private void SetFramerate()
+        {
+#if UNITY_EDITOR
+            if (!workInEditor)
+            {
+                return;
+            }
+#endif
+            Application.targetFrameRate = targetFramerate;
+        }
+    }
+}
